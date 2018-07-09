7f07c7dbf4d7c6d844e0e39355223eb93ec7ac26
WAD123 <wish@wishadesign.com>
2018-07-09 23:49:31 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //database: 'mongodb://bhavye:mlabBhavya93@ds153682.mlab.com:53682/meanauthapp',
-  database:'mongodb://localhost:27017/ocw',
+  database: 'mongodb://bhavye:mlabBhavya93@ds153682.mlab.com:53682/meanauthapp',
+  //database:'mongodb://localhost:27017/ocw',
   secret: 'yoursecret'
 }
